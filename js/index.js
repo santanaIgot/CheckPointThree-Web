aad04e0d7844c2dc666e2d2b48d2e8041cc1a8b1
santanaIgot <santanaigoor01@gmail.com>
2023-05-21 14:35:03 -0300

    Estrutura condição inputNome

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
 const inputNome = document.querySelector("#idNome");
 const labelName = document.querySelector("label[for='idNome']")
+
+inputNome.addEventListener("keyup", () =>{
+    if(inputNome.value.length < 5) {
+        inputNome.setAttribute("style", "outline-color: red;");
+        labelName.setAttribute("style", "color: red;");
+    } else if(inputNome.value.length == 0) {
+        inputNome.setAttribute("style", "outline-color: red;");
+        labelName.setAttribute("style", "color: red;");
+    } else {
+        inputNome.setAttribute("style", "outline-color: green;");
+        labelName.setAttribute("style", "color: green;");
+    }
+})
